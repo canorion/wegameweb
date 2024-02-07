759c1a7d2f04696027409b839211cc5c14736e3e
Can Ürek <can.urek@orioninc.com>
2024-02-07 21:58:03 +0300

    Prod fixes

--- api/config/index.js
+++ api/config/index.js
@@ -2,14 +2,6 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-//const { API_PORT, DB_URI, SECRET_ACCESS_TOKEN, FE_PORT } = process.env;
-
-const API_PORT = process.env.API_PORT || 3000;
-const FE_PORT = process.env.FE_PORT || 3001;
-
-const DB_URI = process.env.DB_URI;
-const SECRET_ACCESS_TOKEN = process.env.SECRET_ACCESS_TOKEN;
-
-
+const { API_PORT, DB_URI, SECRET_ACCESS_TOKEN, FE_PORT } = process.env;
 
 export { API_PORT, DB_URI, SECRET_ACCESS_TOKEN, FE_PORT };
