c12346c6439a3db68454b5b9fa4c791151f44313
Can Ürek <can.urek@orioninc.com>
2024-02-09 16:35:13 +0300

    Audio fix

--- public/games/game2/script.js
+++ public/games/game2/script.js
@@ -12,44 +12,68 @@ const showMessage = (message, type) => {
   messageDiv.innerText = "WINNER IS " + message;
 };
 
-var audioContext;
-
-function loadSound(url) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'arraybuffer';
-
-    xhr.onload = function() {
-        if (xhr.status == 200) {
-            audioContext.decodeAudioData(xhr.response, function(buffer) {
-                playSound(buffer);
-            }, function(e) {
-                console.log('Error decoding audio data: ' + e.err);
-            });
-        }
+(function () {
+  // Check if the browser supports web audio. Safari wants a prefix.
+  if ("AudioContext" in window || "webkitAudioContext" in window) {
+    //////////////////////////////////////////////////
+    // Here's the part for just playing an audio file.
+    //////////////////////////////////////////////////
+    var ButtonPlay = function ButtonPlay(audioBuffer) {
+      var source = context.createBufferSource();
+      source.buffer = audioBuffer;
+      source.connect(context.destination);
+      source.start();
     };
-    xhr.onerror = function() {
-        console.log('XHR error');
-    };
-    xhr.send();
-}
 
-function playSound(buffer) {
-    var source = audioContext.createBufferSource(); // creates a sound source
-    source.buffer = buffer; // tell the source which sound to play
-    source.connect(audioContext.destination); // connect the source to the context's destination (the speakers)
-    source.start(0); // play the source now
-}
+    var soundUrl = "click.mp3";
+    var AudioContext = window.AudioContext || window.webkitAudioContext;
+    var context = new AudioContext(); // Make it crossbrowser
+    var gainNode = context.createGain();
+    gainNode.gain.value = 1; // set volume to 100%
+    var eventButton = document.getElementById("jumpBtn");
+    var yodelBuffer = void 0;
+
+    // The Promise-based syntax for BaseAudioContext.decodeAudioData() is not supported in Safari(Webkit).
+    window
+      .fetch(soundUrl)
+      .then((response) => response.arrayBuffer())
+      .then((arrayBuffer) =>
+        context.decodeAudioData(
+          arrayBuffer,
+          (audioBuffer) => {
+            yodelBuffer = audioBuffer;
+          },
+          (error) => console.error(error)
+        )
+      );
+    
+      eventButton.addEventListener("click", (event) => {
+        return ButtonPlay(yodelBuffer);
+      })
+    //////////////////////////////////////////////////
+    // Here's the part for unlocking the audio context, probably for iOS only
+    //////////////////////////////////////////////////
+
+    function unlock() {
+      console.log("unlocking");
+      // create empty buffer and play it
+      var buffer = context.createBuffer(1, 1, 22050);
+      var source = context.createBufferSource();
+      source.buffer = buffer;
+      source.connect(context.destination);
+
+      // play the file. noteOn is the older version of start()
+      source.start ? source.start(0) : source.noteOn(0);
+
+      // by checking the play state after some time, we know if we're really unlocked
+    }
+    // Try to unlock, so the unmute is hidden when not necessary (in most browsers).
+    unlock();
+  }
+})();
+
 
 btnJump.on('click', function () {
-  
-  if (!audioContext) {
-    audioContext = new (window.AudioContext || window.webkitAudioContext)();
-  }
-  
-  if (audioContext.state === 'suspended') {
-    audioContext.resume();
-  }
   
   const btnJump = $('#jumpBtn');
   btnJump.css('backgroundImage', "url('images/jumpBtnPressed.png')");
@@ -59,8 +83,6 @@ btnJump.on('click', function () {
     btnJump.css('backgroundImage', "url('images/jumpBtn.png')");
     btnJump.prop('disabled', false);
   }, 1000);
-
-  loadSound('click.mp3');
   
   if ("vibrate" in navigator) {
     navigator.vibrate(200);
